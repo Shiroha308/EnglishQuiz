561074d22300d860b3ce8c327e237077b8904ce3
Shiroha308 <68191116+Shiroha308@users.noreply.github.com>
2022-09-07 22:03:49 +0900

    Update debug.php

--- debug.php
+++ debug.php
@@ -9,4 +9,18 @@
 <?php
   echo "<p>動作確認その2</p>";
 ?>
+   <article>
+            <div class="wrapper">
+                <div class="namearea">
+                    <span>名前：</span>
+                    <a class="username"><?php echo "username"; ?></a>
+                    <time><?php echo "postDate"; ?></time>
+                </div>
+                <p class="comment"><?php echo "comment"; ?></p>
+            </div>
+   </article>
+  
+  
+  
+
 </html>
