ea003c55ed09629e1611d0ce62c0a12c40373a36
Shiroha308 <68191116+Shiroha308@users.noreply.github.com>
2022-09-08 15:09:37 +0900

    Update environment.php

--- environment.php
+++ environment.php
@@ -1,6 +1,6 @@
 <?php
 //
-//DB接続専用のファイルです。
+//loginデータベースへアクセス
 //
 
 define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
@@ -27,11 +27,50 @@ try{
     echo '接続失敗'. $e->getMessage();
     }
 }
-//成功と画面上に表示される
-connect();
 
-//①フォームからデータを渡す
-//②受け取ってvalidationを確認
-//③ユーザ情報を登録する
+//
+//openchatデータベースへアクセス
+//
+    $pdo = null;
+    $stmt = null;
+    $error_messages = array();
+    //openchat-tableへのアクセスを行う
+    try{
+        $pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_e8f3a7d35b48f46', "b107445cca4e99", "a12347dc");
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }    
+    //DBからのデータを格納する配列を用意する
+    $comment_array = array();
 
+    //フォームを打ち込んだ時、DBへデータを追加できるようにする
+    if(!empty($_POST["submitButton"])){
+
+    //名前が入力されているか確認
+    if(empty($_POST["username"])){
+        echo "名前を入力してください。";
+        $error_messages["username"] = "";
+    }    
+    if(empty($_POST["comment"])){
+        echo "コメントを入力してください。";
+        $error_messages["comment"] = "";
+    }       
+
+    if(empty($error_messages)){
+    //データを送信する
+        $postDate = date("Y-m-d H:i:s");
+    //PHP PDO公式ドキュメントからコピー
+        $stmt = $pdo->prepare("INSERT INTO `openchat-table` (`username`, `comment`, `postDate`) VALUES (:username, :comment, :postDate);");
+        $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+        $stmt->bindParam(':comment', $_POST['comment'], PDO::PARAM_STR);
+        $stmt->bindParam(':postDate', $postDate, PDO::PARAM_STR);
+    //executeで実行する
+        $stmt->execute();
+    }
+    }   
+    //DBからデータを取集する
+    $sql = "SELECT `id`,`username`,`comment`,`postDate` FROM `openchat-table`;";
+    $comment_array = $pdo->query($sql);
+    //DBの接続を閉じる
+    $pdo = null;
 ?>
