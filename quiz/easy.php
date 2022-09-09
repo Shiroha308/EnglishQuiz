cadabf950eca98e87c05b57f59f7c6f9f7d1c886
Shiroha308 <68191116+Shiroha308@users.noreply.github.com>
2022-09-09 09:35:08 +0900

    Update easy.php

--- quiz/easy.php
+++ quiz/easy.php
@@ -1,6 +1,6 @@
 <?php
     include ( dirname(__FILE__) . '/../debug.php');
-    include ( dirname(__FILE__) . '/../environment.php');
+    include ( dirname(__FILE__) . '/../database.php');
 ?>
 
 <!DOCTYPE html>
